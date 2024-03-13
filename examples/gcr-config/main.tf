37ead46d5d522f9caaacbf751c7daed1d760da5d
Jonathan Nappi <jnappi@uptycs.com>
2024-03-13 09:05:12 -0400

    add aws_instance_roles to example and README

--- examples/gcr-config/main.tf
+++ examples/gcr-config/main.tf
@@ -1,11 +1,12 @@
 module "gcr-config" {
-  source            = "uptycslabs/gcr-config/google"
-  
-  uptycs_aws_account_id = "Copy/Paste From the Uptycs UI"
-  gcp_project_id        = "Your GCP Project ID"
-  service_account_name  = "uptycs-gcr-integration"
+  source = "uptycslabs/gcr-config/google"
+
+  uptycs_aws_account_id     = "Copy/Paste From the Uptycs UI"
+  uptycs_aws_instance_roles = ["Copy/Paste From the Uptycs UI"]
+  gcp_project_id            = "Your GCP Project ID"
+  service_account_name      = "uptycs-gcr-integration"
 
   # Copy the AWS Account ID from Uptycs UI
   # Uptycs' UI : "Configurations"->"Registry Configuration"->"ADD REGISTRY"
-  integration_name   = "A unique name for this integration"
+  integration_name = "A unique name for this integration"
 }
